07b3f58c09e8789e7f8152048a60f2c74f58cc0f
Brenno Silva <b.renno.silva@hotmail.com>
2018-09-30 16:31:06 -0300

    Criação da Classe User

---
+++ src/main/java/br/com/matrix/idioma/model/User.java
@@ -0,0 +1,19 @@
+package br.com.matrix.idioma.model;
+
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+	
+	private long id;
+	private String login;
+	private String password;
+	private String email;
+	private String name;
+
+}
