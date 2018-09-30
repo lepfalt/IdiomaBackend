446f1cafa3833b7c41683562133131c2c97a1d17
Patric Freitas <patricfreitas@hotmail.com>
2018-09-30 17:43:46 -0300

    Criacao MarkingService

---
+++ src/main/java/br/com/matrix/idioma/service/MarkingService.java
@@ -0,0 +1,38 @@
+package br.com.matrix.idioma.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.matrix.idioma.model.Audio;
+import br.com.matrix.idioma.model.Marking;
+import br.com.matrix.idioma.model.User;
+import br.com.matrix.idioma.repository.MarkingRepository;
+
+public class MarkingService {
+	
+	@Autowired
+	MarkingRepository markingRepository;
+	
+	public Marking create(Marking marking) {
+		return markingRepository.save(marking);
+	}
+	
+	public Optional<Marking> findById(Long id) {
+		return markingRepository.findById(id);
+	}
+	
+	public List<Marking> findByUserAndAudio(User user, Audio audio){
+		return markingRepository.findyByUserAndAudio(user, audio);
+	}
+	
+	public Marking update(Marking marking) {
+		return markingRepository.save(marking);
+	}
+	
+	public void deleteById(Long id) {
+		markingRepository.deleteById(id);
+	}
+
+}
