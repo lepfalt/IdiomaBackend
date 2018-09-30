cf2f5771cec1064c6d66dd90e0e60ea4f6031855
helio <helio_dantas@outlook.com>
2018-09-30 16:59:56 -0300

    repository2

--- src/main/java/br/com/matrix/idioma/repository/AudioRepository.java
+++ src/main/java/br/com/matrix/idioma/repository/AudioRepository.java
@@ -1,9 +1,10 @@
 package br.com.matrix.idioma.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.matrix.idioma.model.Audio;
-
+@Repository
 public interface AudioRepository  extends JpaRepository<Audio, Long> {
 
 	Audio findyByAudio(long id);
--- src/main/java/br/com/matrix/idioma/repository/UserRepository.java
+++ src/main/java/br/com/matrix/idioma/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package br.com.matrix.idioma.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.matrix.idioma.model.User;
 
+@Repository
 public interface UserRepository  extends JpaRepository<User, Long> {
 	 
 	User findByUser(Long id);
